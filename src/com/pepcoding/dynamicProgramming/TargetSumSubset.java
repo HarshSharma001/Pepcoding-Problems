bc88ddc8967d76ea5295da9786c71b71a2a2f1f8
HarshSharma001 <geetaharshit1968@gmail.com>
2021-09-11 23:50:29 +0530

    11th Sep 2021

--- src/com/pepcoding/dynamicProgramming/TargetSumSubset.java
+++ src/com/pepcoding/dynamicProgramming/TargetSumSubset.java
@@ -7,33 +7,55 @@ public class TargetSumSubset {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         int target = sc.nextInt();
-        System.out.println(useRecursion(0, n-1, target, arr));
+//        System.out.println(useRecursion(0, n - 1, target, arr));
+        System.out.println(useTabulation(n, target, arr));
     }
 
-    private static boolean useRecursion(int sum, int n, int target, int[] arr){
+    private static boolean useRecursion(int sum, int n, int target, int[] arr) {
         //Base Case
-        if(n < 0) {
-            if(sum == target) return true;
+        if (n < 0) {
+            if (sum == target) return true;
             return false;
         }
-
-        if(sum > target) return false;
-
-        if(sum == target) return true;
+        if (sum > target) return false;
+        if (sum == target) return true;
 
         //faith
         boolean ans1 = useRecursion(sum + arr[n], n - 1, target, arr); //aayega
-        if(ans1){
+        if (ans1) {
             return ans1;
-        }
-        else if(!ans1){
+        } else if (!ans1) {
             boolean ans2 = useRecursion(sum, n - 1, target, arr); //nhi aayega
-            if(ans2) return ans2;
+            if (ans2) return ans2;
         }
         return false;
+    }
+
+    private static boolean useTabulation(int n, int target, int[] arr){
+        boolean[][] dp = new boolean[n + 1][target + 1];    //storage
+        dp[0][0] = true;    //meaning
+
+        for(int i=0; i<=n; i++){
+            if(i >= 1){
+                for(int j=0; j<=target; j++){
+                    int currentTarget = j;
+                    //When the current player decides to bat
+                    if(currentTarget >= arr[i-1]){
+                        int remainingScore = currentTarget - arr[i-1];
+                        if(remainingScore >= 0)
+                        if(dp[i-1][remainingScore]) dp[i][j] = true;
+                    }
+                    //When the current player decides not to bat
+                    if(!dp[i][j]){
+                        if(dp[i-1][j]) dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return dp[n][target];
     }
 }
